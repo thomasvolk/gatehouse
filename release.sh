8d16cefe84076f972b18d40bcf2d4636ac9b56c5
Thomas Volk (thomas-N24-25JU) <thomas@my-volk.de>
2018-06-16 22:31:06 +0200

    check for achive

--- release.sh
+++ release.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+ARCHIVE=gatehouse.tar.gz
+
+if [ ! -f $ARCHIVE ]; then
+  echo "archive $ARCHIVE not found!"
+  echo "run ./build.sh first"
+  exit 1
+fi
+
 docker build -f Dockerfile.release -t gatehouse-release .
