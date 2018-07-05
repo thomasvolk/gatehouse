9684c9183ed935c9f283c73ee6b51368138d85a7
Thomas Volk (tuxedo-N24-25JU) <thomas@my-volk.de>
2018-07-05 08:34:33 +0200

    install node in the docker container and build the assets there

--- build.sh
+++ build.sh
@@ -4,7 +4,6 @@ set -e
 
 VERSION=$(cat mix.exs | grep -P 'version: ".+"' | grep -Po '(?<=)(\d+\.\d\.\d)')
 echo "build release '$VERSION'"
-(cd assets && npm install && ./node_modules/brunch/bin/brunch build) 
 docker build -f Dockerfile.build -t gatehouse-build .
 docker rm gatehouse-build || true
 docker create --name gatehouse-build gatehouse-build
