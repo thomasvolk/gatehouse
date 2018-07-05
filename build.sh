140981953e7028d830b9de78701120595ee70865
Thomas Volk (tuxedo-N24-25JU) <thomas@my-volk.de>
2018-07-05 08:19:55 +0200

    add brunch to build process

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ set -e
 
 VERSION=$(cat mix.exs | grep -P 'version: ".+"' | grep -Po '(?<=)(\d+\.\d\.\d)')
 echo "build release '$VERSION'"
+(cd assets && npm install && ./node_modules/brunch/bin/brunch build) 
 docker build -f Dockerfile.build -t gatehouse-build .
 docker rm gatehouse-build || true
 docker create --name gatehouse-build gatehouse-build
