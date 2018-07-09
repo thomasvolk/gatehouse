9687079d20b6bc7bf7ee9e05510b4b6f2989dc55
Thomas Volk (tuxedo-N24-25JU) <thomas@my-volk.de>
2018-07-09 22:59:12 +0200

    upload deb package

--- deploy-deb.sh
+++ deploy-deb.sh
@@ -2,9 +2,10 @@
 
 set -e
 
+VERSION=$(cat mix.exs | grep -P 'version: ".+"' | grep -Po '(?<=)(\d+\.\d\.\d)')
 PACKAGE_NAME=gatehouse
 NAMESPACE=gatehouse
-VERSION=$(cat mix.exs | grep -P 'version: ".+"' | grep -Po '(?<=)(\d+\.\d\.\d)')
+PACKAGE=gatehouse-$VERSION.deb
 
-curl -T gatehouse-$VERSION.deb -uthomasvolk:$DEBIAN_REGISTRY_PASSWORD \
-  $DEBIAN_REGISTRY_URL/$PACKAGE_NAME/$VERSION/$NAMESPACE;deb_distribution=wheezy;deb_component=main;deb_architecture=i386,amd64
+curl -T $PACKAGE -uthomasvolk:$DEBIAN_REGISTRY_PASSWORD \
+  "$DEBIAN_REGISTRY_URL/$PACKAGE_NAME/$VERSION/$PACKAGE;deb_distribution=wheezy;deb_component=main;deb_architecture=i386,amd64"
