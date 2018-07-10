f2c1df62ec35e7532fcc74a3774f3b9af6881dc4
Thomas Volk (tuxedo-N24-25JU) <thomas@my-volk.de>
2018-07-10 20:06:13 +0200

    remove deb package

--- build.sh
+++ build.sh
@@ -8,4 +8,3 @@ docker build -f Dockerfile.build -t gatehouse-build .
 docker rm gatehouse-build || true
 docker create --name gatehouse-build gatehouse-build
 docker cp gatehouse-build:_build/prod/rel/gatehouse/releases/$VERSION/gatehouse.tar.gz .
-docker cp gatehouse-build:_build/gatehouse-$VERSION.deb .
