8ba7675eee10c10b28a918b694613fed2a84b36f
Thomas Volk (Inspiron-7437) <thomas@my-volk.de>
2018-08-07 21:30:37 +0200

    password form

--- assets/js/admin/components/PrincipalDetails.jsx
+++ assets/js/admin/components/PrincipalDetails.jsx
@@ -3,7 +3,22 @@ import React from "react"
 export default class PrincipalDetails extends React.Component {
   render() {
     return (
-        <div>Principal Details</div>
+        <div className="card">
+          <div className="card-body">
+            <h3 className="card-title">test@example.com</h3>
+            <form>
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <input type="password" id="password" className="form-control" placeholder="Password">
+              </input>
+              <label htmlFor="password-repeat">Password repeat</label>
+              <input type="password" id="password-repeat" className="form-control" placeholder="Password repeat">
+              </input>
+            </div>
+            <button type="submit" className="btn btn-primary">Update</button>
+            </form>
+          </div>        
+        </div>
        )
   }
 }
--- assets/js/admin/components/PrincipalList.jsx
+++ assets/js/admin/components/PrincipalList.jsx
@@ -4,11 +4,14 @@ import React from "react"
 export default class PrincipalList extends React.Component {
   render() {
     return (
-      <ul className="list-group">
-        <li className="list-group-item">admin@example.com</li>
-        <li className="list-group-item">test@example.com</li>
-        <li className="list-group-item">foo@example.com</li>
-      </ul>
+      <div>
+        <ul className="list-group">
+          <li className="list-group-item">admin@example.com</li>
+          <li className="list-group-item">test@example.com</li>
+          <li className="list-group-item">foo@example.com</li>
+          <li className="list-group-item">bar@example.com</li>
+        </ul>
+      </div>
        )
   }
 }
