f7c851fdb0fecc1f6e195183a99aa2bb52f8e662
Thomas Volk (Inspiron-7437) <thomas@my-volk.de>
2018-08-12 22:35:02 +0200

    add create button

--- assets/js/admin/components/AdminApp.jsx
+++ assets/js/admin/components/AdminApp.jsx
@@ -2,6 +2,7 @@
 import React from "react"
 import T from 'i18n-react'
 import PrincipalList from "./PrincipalList"
+import CreatePrincipal from "./CreatePrincipal"
 import Principal from "./Principal"
 
 export default class AdminApp extends React.Component {
@@ -10,6 +11,7 @@ export default class AdminApp extends React.Component {
         <div className="container-fluid">
           <div className="row">
             <div className="col-xs-4">
+              <CreatePrincipal/>
               <PrincipalList/>
             </div>
             <div className="col-xs-8">
---
+++ assets/js/admin/components/CreatePrincipal.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default class CreatePrincipal extends React.Component {
+  render() {
+    return (
+       <button type="button" className="btn btn-primary">create principal</button>
+       )
+  }
+}
