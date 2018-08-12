bb48a0e5682c48710aade26a462fea6db564e10c
Thomas Volk (Inspiron-7437) <thomas@my-volk.de>
2018-08-12 22:45:23 +0200

    add grid for principal

--- assets/js/admin/components/Principal.jsx
+++ assets/js/admin/components/Principal.jsx
@@ -8,10 +8,16 @@ export default class Principal extends React.Component {
     return (
         <div className="card">
           <div className="card-body">
+          <div className="container-fluid">
+              <div className="row">
+                <div className="col-xs-8">
+                  <Email/>
+                </div>
+              </div>
+            </div>
             <div className="container-fluid">
               <div className="row">
                 <div className="col-xs-6">
-                  <Email/>
                   <Password/>
                 </div>
                 <div className="col-xs-6">
