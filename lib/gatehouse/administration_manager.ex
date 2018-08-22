aa4d732e40c0717c4c08338915b93abd4419d885
Thomas Volk (Inspiron-7437) <thomas@my-volk.de>
2018-08-22 08:31:01 +0200

    remove repo parameter fo administration_manager

--- lib/gatehouse/administration_manager.ex
+++ lib/gatehouse/administration_manager.ex
@@ -2,33 +2,34 @@ defmodule Gatehouse.AdministrationManager do
     import Ecto.Query, only: [from: 2]
     alias Gatehouse.Principal
     alias Gatehouse.Role
+    alias Gatehouse.Repo
 
-    def get_principal_by_id(repo, id) do
-        repo.get_by(Principal, id: id)
+    def get_principal_by_id(id) do
+        Repo.get_by(Principal, id: id)
     end
     
-    def get_roles_with_principals(repo) do
+    def get_roles_with_principals() do
         query = from r in Role, preload: [:principals]
-        repo.all(query)
+        Repo.all(query)
     end
 
 
-    def get_principals(repo) do
-        repo.all(Principal)
+    def get_principals() do
+        Repo.all(Principal)
     end
 
-    def update_pricipal_to_role_relation(repo, {principal_id, role_id, active}) do
-        repo.transaction(fn ->
-        principal = get_principal_by_id(repo, principal_id)
-        role = repo.get_by(Role, id: role_id)
-        principal = principal |> repo.preload(:roles)
-        roles = if active do
-            Enum.concat(principal.roles, [role])
-        else
-            Enum.filter(principal.roles, fn r -> r.id != role.id end)
-        end
-        changeset = Ecto.Changeset.change(principal) |> Ecto.Changeset.put_assoc(:roles, roles)
-        repo.update!(changeset)
+    def update_pricipal_to_role_relation({principal_id, role_id, active}) do
+        Repo.transaction(fn ->
+            principal = get_principal_by_id(principal_id)
+            role = Repo.get_by(Role, id: role_id)
+            principal = principal |> Repo.preload(:roles)
+            roles = if active do
+                Enum.concat(principal.roles, [role])
+            else
+                Enum.filter(principal.roles, fn r -> r.id != role.id end)
+            end
+            changeset = Ecto.Changeset.change(principal) |> Ecto.Changeset.put_assoc(:roles, roles)
+            Repo.update!(changeset)
         end)
     end
 
--- lib/gatehouse_web/controllers/administration_controller.ex
+++ lib/gatehouse_web/controllers/administration_controller.ex
@@ -13,14 +13,14 @@ defmodule GatehouseWeb.AdministrationController do
   end
 
   def principal_list(conn, _params) do
-    principals = AdministrationManager.get_principals(Gatehouse.Repo)
+    principals = AdministrationManager.get_principals()
       |> Enum.map(&map_principal/1)
     json conn, principals
   end
 
   def principal_details(conn, %{"id" => id} = _params) do
-    principal = map_principal(AdministrationManager.get_principal_by_id(Gatehouse.Repo, id))
-    roles = AdministrationManager.get_roles_with_principals(Gatehouse.Repo)
+    principal = map_principal(AdministrationManager.get_principal_by_id(id))
+    roles = AdministrationManager.get_roles_with_principals()
                 |> Enum.map(fn r -> %{id: r.id, 
                           name: r.name, 
                           active: r.principals |> Enum.count(fn p -> p.id == String.to_integer(id) end) > 0 } 
@@ -29,7 +29,7 @@ defmodule GatehouseWeb.AdministrationController do
   end
 
   def update_role_relation(conn, %{"principal_id" => principal_id, "role_id" => role_id, "active" => active}) do
-    AdministrationManager.update_pricipal_to_role_relation(Gatehouse.Repo,
+    AdministrationManager.update_pricipal_to_role_relation(
        {principal_id, role_id, active})
        json conn, "OK" 
   end
