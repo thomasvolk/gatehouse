cc3a5f4ed8884f6999e9cdec927de7b90f6b224b
Thomas Volk (Inspiron-7437) <thomas@my-volk.de>
2018-08-22 22:43:48 +0200

    move mapping code into the manager

--- lib/gatehouse/administration_manager.ex
+++ lib/gatehouse/administration_manager.ex
@@ -4,23 +4,19 @@ defmodule Gatehouse.AdministrationManager do
     alias Gatehouse.Role
     alias Gatehouse.Repo
 
-    def get_principal_by_id(id) do
-        Repo.get_by(Principal, id: id)
+    defp map_principal(p) do
+        %{ id: p.id, 
+            email: p.email
+         } 
     end
-    
-    def get_roles_with_principals() do
-        query = from r in Role, preload: [:principals]
-        Repo.all(query)
-    end
-
 
     def get_principals() do
-        Repo.all(Principal)
+        Repo.all(Principal) |> Enum.map(&map_principal/1)
     end
 
     def update_pricipal_to_role_relation(principal_id, role_id, active) do
         Repo.transaction(fn ->
-            principal = get_principal_by_id(principal_id)
+            principal = Repo.get_by(Principal, id: principal_id)
             role = Repo.get_by(Role, id: role_id)
             principal = principal |> Repo.preload(:roles)
             roles = if active do
@@ -31,6 +27,17 @@ defmodule Gatehouse.AdministrationManager do
             changeset = Ecto.Changeset.change(principal) |> Ecto.Changeset.put_assoc(:roles, roles)
             Repo.update!(changeset)
         end)
+    end
+
+
+    def principal_with_roles_selection_list(id) do
+        principal = map_principal(Repo.get_by(Principal, id: id))
+        roles = Repo.all(from r in Role, preload: [:principals])
+                    |> Enum.map(fn r -> %{id: r.id, 
+                                name: r.name, 
+                                active: r.principals |> Enum.count(fn p -> p.id == String.to_integer(id) end) > 0 } 
+                    end)
+        Map.put(principal, :roles_selection_list, roles) 
     end
 
     def generate_random_password(length \\ 16), do: RandomBytes.base62(length)
--- lib/gatehouse_web/controllers/administration_controller.ex
+++ lib/gatehouse_web/controllers/administration_controller.ex
@@ -6,29 +6,18 @@ defmodule GatehouseWeb.AdministrationController do
     render conn, "index.html", layout: {GatehouseWeb.LayoutView, "administration.html"}
   end
 
-  defp map_principal(p) do
-    %{ id: p.id, 
-        email: p.email
-     } 
-  end
-
   def principal_list(conn, _params) do
     principals = AdministrationManager.get_principals()
-      |> Enum.map(&map_principal/1)
     json conn, principals
   end
 
   def principal_with_roles_selection_list(conn, %{"id" => id} = _params) do
-    principal = map_principal(AdministrationManager.get_principal_by_id(id))
-    roles = AdministrationManager.get_roles_with_principals()
-                |> Enum.map(fn r -> %{id: r.id, 
-                          name: r.name, 
-                          active: r.principals |> Enum.count(fn p -> p.id == String.to_integer(id) end) > 0 } 
-                end)
-    json conn, Map.put(principal, :roles_selection_list, roles) 
+    principal = AdministrationManager.principal_with_roles_selection_list(id)
+    json conn, principal
   end
 
-  def update_role_relation(conn, %{"principal_id" => principal_id, "role_id" => role_id, "active" => active}) do
+  def update_role_relation(conn, %{"principal_id" => principal_id, 
+                                 "role_id" => role_id, "active" => active}) do
     AdministrationManager.update_pricipal_to_role_relation(principal_id, role_id, active)
        json conn, true
   end
