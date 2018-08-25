1990fe218871e09495bdb1553ffebd15dcbf783f
Thomas Volk (Inspiron-7437) <thomas@my-volk.de>
2018-08-25 20:52:01 +0200

    store csrf token

--- assets/js/admin/server.jsx
+++ assets/js/admin/server.jsx
@@ -1,6 +1,17 @@
+function setCSRFToken(token) {
+    sessionStorage.setItem("csrf-token", token)
+}
+
+function getCSRFToken() {
+    return sessionStorage.getItem("csrf-token")
+}
+
 function get(url) {
     return fetch(url)
-        .then(response => response.json())
+        .then(response => {
+            setCSRFToken(response.headers.get("x-csrf-token"))
+            return response.json()
+        })
         .catch(ex => {
         alert("ERROR: " + ex);
     })
@@ -10,9 +21,13 @@ function put(url, body) {
     return fetch(url, {
         method: 'PUT',
         headers: {
-          'Content-Type': 'application/json; charset=utf-8'
+          'Content-Type': 'application/json; charset=utf-8',
+          'x-csrf-token': getCSRFToken()
         },
         body: JSON.stringify(body)
+    }).then(response => {
+        setCSRFToken(response.headers.get("x-csrf-token"))
+        return response.json()
     });
 }
 
