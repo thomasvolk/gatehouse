d367931e164d0a8adfb4ebfdc5e7d0099fceb131
Thomas Volk (Inspiron-7437) <thomas@my-volk.de>
2018-08-30 18:46:33 +0200

    add alertbox for password update

--- assets/js/admin/components/CreatePrincipal.jsx
+++ assets/js/admin/components/CreatePrincipal.jsx
@@ -33,19 +33,19 @@ export default class CreatePrincipal extends React.Component {
     const errorMessage = this.state.errorMessage
     return (
       <div>
-      <AlertBox errorMessage={errorMessage}/>
-       <div>
-         <h2>Create Principal</h2>
-         <form onSubmit={(e) => this.handleSubmit(e)}>
-         <label htmlFor="password">Password</label>
-            <input type="text" id="email" className="form-control" 
-              placeholder="Email" onChange={(e) => this.updateEmail(e)}>
-            </input>
-            <button type="button" className="btn btn-secundary"
-              onClick={() => this.onCancel()}>Cancel</button>
-            <button type="submit" className="btn btn-primary">Create</button>
-         </form>
-       </div>
+        <AlertBox errorMessage={errorMessage}/>
+        <div>
+          <h2>Create Principal</h2>
+          <form onSubmit={(e) => this.handleSubmit(e)}>
+          <label htmlFor="password">Password</label>
+              <input type="text" id="email" className="form-control" 
+                placeholder="Email" onChange={(e) => this.updateEmail(e)}>
+              </input>
+              <button type="button" className="btn btn-secundary"
+                onClick={() => this.onCancel()}>Cancel</button>
+              <button type="submit" className="btn btn-primary">Create</button>
+          </form>
+        </div>
        </div>
       )
   }
