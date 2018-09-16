6a7291ae6524d6f83c2c8bdcb9806540801c566b
Thomas Volk (Inspiron-7437) <thomas@my-volk.de>
2018-09-16 11:19:43 +0200

    add redirect to react router

--- assets/js/admin/admin_app.jsx
+++ assets/js/admin/admin_app.jsx
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom"
 import React from "react"
 import "./i18n"
-import { Router, Route, Redirect } from 'react-router'
+import { Router, Route, Redirect, Switch } from 'react-router'
 import createHashHistory from "history/createHashHistory"
 import PrincipalAdmin from "./components/principal/PrincipalAdmin"
 import AlertBox from "./components/AlertBox"
@@ -12,7 +12,10 @@ ReactDOM.render(
   <Router history={history}>
     <div>
       <AlertBox/>
+      <Switch>
+      <Redirect exact path="/" to="/principal"/>
       <Route path="/principal" component={PrincipalAdmin}/>
+      </Switch>
     </div>
   </Router>,
   document.getElementById("admin-app")
