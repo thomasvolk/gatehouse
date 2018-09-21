0217aea135b222560113bb8d60a5b04c48357cc9
Thomas Volk (Inspiron-7437) <thomas@my-volk.de>
2018-09-21 23:47:56 +0200

    refactor dispatcher

--- assets/js/admin/dispatcher.jsx
+++ assets/js/admin/dispatcher.jsx
@@ -20,13 +20,26 @@ class Observable {
  
 }
 
-export default {
-    principalSelected: new Observable(),
-    principalChanged: new Observable(),
-    principalCreated: new Observable(),
-    principalDeleted: new Observable(),
-    roleCreated: new Observable(),
-    roleDeleted: new Observable(),
-    roleSelected: new Observable(),
-    onError: new Observable()
+class Dispatcher extends Observable {
+    constructor() {
+        super()
+        this.principalSelected = this.observable('principalSelected')
+        this.principalChanged = this.observable('principalChanged')
+        this.principalCreated = this.observable('principalCreated')
+        this.principalDeleted = this.observable('principalDeleted')
+        this.roleCreated = this.observable('roleCreated')
+        this.roleDeleted = this.observable('roleDeleted')
+        this.roleSelected = this.observable('roleSelected')
+        this.onError = new Observable()
+    }
+
+    observable(eventType) {
+        const o = new Observable()
+        o.addObserver( (item) => this.update({ eventType: eventType, message: item}) )
+        return o
+    }
 }
+
+const DISPATCHER = new Dispatcher()
+
+export default DISPATCHER
