a11170e0b55bca041f37d07388c0ebc20070fed6
Thomas Volk (Inspiron-7437) <thomas@my-volk.de>
2018-09-23 10:01:22 +0200

    add password show component

--- assets/js/admin/components/principal/CreatePrincipal.jsx
+++ assets/js/admin/components/principal/CreatePrincipal.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import ShowPassword from './detail/ShowPassword'
 import Server from "../../server"
 import Dispatcher from "../../dispatcher"
 
@@ -6,16 +7,16 @@ export default class CreatePrincipal extends React.Component {
   
   constructor(props) {
     super(props)
-    this.state = { password: undefined, showPassword: false, email: "" }
+    this.state = { password: undefined, email: "" }
   }
   
   onClose() {
-    this.setState({ password: undefined })
+    this.state = { password: undefined, email: "" }
     this.props.close()
   }
 
   updateEmail(event) {
-    this.setState({email: event.target.value, showPassword: false})
+    this.setState({email: event.target.value})
   }
 
   handleSubmit(event) {
@@ -25,16 +26,6 @@ export default class CreatePrincipal extends React.Component {
           this.setState({password: principal.password})
         })
     event.preventDefault()
-  }
-
-  onShowPassword(showPassword) {
-    this.setState({showPassword: showPassword})
-  }
-
-  onCopy(id) {
-    var copyText = document.getElementById(id)
-    copyText.select()
-    document.execCommand("copy")
   }
   
   renderEmailForm() {
@@ -54,31 +45,20 @@ export default class CreatePrincipal extends React.Component {
     )
   }
 
-  renderPasswordView(showPassword, password) {
-    const showPwdButtonText = showPassword ? "Hide password" : "Show password"
-    const password_field_id = "principal_password"
-    const pwd = showPassword ? <div>
-        <input type="text" readOnly value={password} id={password_field_id}></input>
-        <button type="button" className="btn btn-secundary"
-                onClick={() => this.onCopy(password_field_id)}>copy</button>
-      </div> : <div></div>
-
+  renderPasswordView(password) {
     return (
       <div>
          <button type="button" className="btn btn-secundary"
               onClick={() => this.onClose()}>Close</button>
-         <button type="button" className="btn btn-primary"
-                onClick={() => this.onShowPassword(!showPassword)}>{showPwdButtonText}</button>
-         {pwd}
+         <ShowPassword password={password}/>
       </div>
     )
   }
 
   render() {
     const password = this.state.password
-    const showPassword = this.state.showPassword
     if(password) {
-      return this.renderPasswordView(showPassword, password)
+      return this.renderPasswordView(password)
     }
     else {
       return this.renderEmailForm()
---
+++ assets/js/admin/components/principal/detail/ShowPassword.jsx
@@ -0,0 +1,39 @@
+import React from "react"
+
+export default class ShowPassword extends React.Component {
+  
+  constructor(props) {
+    super(props)
+    this.state = { showPassword: false }
+  }
+
+  onShowPassword(showPassword) {
+    this.setState({showPassword: showPassword})
+  }
+
+  onCopy(id) {
+    var copyText = document.getElementById(id)
+    copyText.select()
+    document.execCommand("copy")
+  }
+
+  render() {
+    const password = this.props.password
+    const showPassword = this.state.showPassword
+    const showPwdButtonText = showPassword ? "Hide password" : "Show password"
+    const password_field_id = "principal_password"
+    const pwd = showPassword ? <div>
+        <input type="text" readOnly value={password} id={password_field_id}></input>
+        <button type="button" className="btn btn-secundary"
+                onClick={() => this.onCopy(password_field_id)}>copy</button>
+      </div> : <div></div>
+
+    return (
+      <span>
+         <button type="button" className="btn btn-primary"
+                onClick={() => this.onShowPassword(!showPassword)}>{showPwdButtonText}</button>
+         {pwd}
+      </span>
+    )
+  }
+}
