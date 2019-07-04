8cbbfa8778a9ad754d652f303e4e1226635653aa
Thomas Volk (Inspiron-7437) <thomas@my-volk.de>
2019-07-04 21:42:48 +0200

    bugfixes

--- lib/gatehouse_web/endpoint.ex
+++ lib/gatehouse_web/endpoint.ex
@@ -1,8 +1,6 @@
 defmodule GatehouseWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :gatehouse
 
-  socket "/socket", GatehouseWeb.UserSocket
-
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phoenix.digest
