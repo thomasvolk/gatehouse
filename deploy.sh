6af8afc6105592b4baddf6a93f2fbb76bd0e3aa8
Thomas Volk (dell) <thomas@my-volk.de>
2019-08-05 22:51:04 +0200

    change repository name

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,11 @@
 
 set -e
 
+REPOSITORY=pdr
 PACKAGE_NAME=gatehouse
 NAMESPACE=gatehouse
 VERSION=latest
-DOCKER_REGISTRY_SERVER=thomasvolk-docker-public.bintray.io
+DOCKER_REGISTRY_SERVER=thomasvolk-docker-$REPOSITORY.bintray.io
 
 docker login -u $DOCKER_REGISTRY_USER -p $DOCKER_REGISTRY_PASSWORD $DOCKER_REGISTRY_SERVER
 docker tag gatehouse-release $DOCKER_REGISTRY_SERVER/$NAMESPACE/$PACKAGE_NAME:$VERSION
