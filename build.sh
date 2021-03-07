099df7d95c2f73d07928791cc97769fbfc4bfe1b
Thomas Volk (MacBook) <info@thomasvolk.de>
2021-03-07 21:31:01 +0100

    fix release archive creation

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ echo "build release ..."
 docker build -f Dockerfile.build -t gatehouse-build .
 docker rm gatehouse-build || true
 docker create --name gatehouse-build gatehouse-build
-docker cp gatehouse-build:_dist/gatehouse.tar.gz .
+docker cp gatehouse-build:_build/gatehouse.tar.gz .
